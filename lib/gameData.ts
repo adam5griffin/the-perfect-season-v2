0abef004010fd643a4fe0764f4652c752e35b1f4
adam5griffin <adam5griffin@gmail.com>
2026-06-06 18:23:42 -0700

    Update gameData.ts

--- lib/gameData.ts
+++ lib/gameData.ts
@@ -6,24 +6,45 @@ export const sports = {
     perfectRecord: '82-0',
     reSpins: 3,
     roster: ['PG', 'SG', 'SF', 'PF', 'C', '6th Man'],
-    teams: ['Lakers', 'Celtics', 'Bulls', 'Warriors', 'Spurs', 'Heat', 'Knicks', '76ers'],
-    eras: ['1960s', '1970s', '1980s', '1990s', '2000s', '2010s', '2020s'],
   },
+
   nfl: {
     name: 'NFL',
     perfectRecord: '17-0',
     reSpins: 6,
-    roster: ['QB', 'RB', 'WR1', 'WR2', 'TE', 'FLEX', 'Offensive Line', 'Team Defense + Decade'],
-    teams: ['Patriots', 'Cowboys', '49ers', 'Steelers', 'Chiefs', 'Packers', 'Ravens', 'Bears'],
-    eras: ['1970s', '1980s', '1990s', '2000s', '2010s', '2020s'],
+    roster: [
+      'QB',
+      'RB',
+      'WR1',
+      'WR2',
+      'TE',
+      'FLEX',
+      'Offensive Line',
+      'Team Defense'
+    ],
   },
+
   mlb: {
     name: 'MLB',
     perfectRecord: '162-0',
     reSpins: 5,
-    roster: ['C', '1B', '2B', '3B', 'SS', 'LF', 'CF', 'RF', 'DH', 'SP1', 'SP2', 'SP3', 'SP4', 'SP5', 'Bullpen + Decade'],
-    teams: ['Yankees', 'Dodgers', 'Red Sox', 'Cardinals', 'Giants', 'Braves', 'Mets', 'Cubs'],
-    eras: ['1960s', '1970s', '1980s', '1990s', '2000s', '2010s', '2020s'],
+    roster: [
+      'C',
+      '1B',
+      '2B',
+      '3B',
+      'SS',
+      'LF',
+      'CF',
+      'RF',
+      'DH',
+      'SP1',
+      'SP2',
+      'SP3',
+      'SP4',
+      'SP5',
+      'Bullpen'
+    ],
   },
 } as const;
 
